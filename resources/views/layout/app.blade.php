9557490a25aece2bb0376d1a4d763928d3f74f5a
RILZSATBOI <fahrilnursyamsi2004@gmail.com>
2025-12-06 10:59:40 +0700

    Message

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -22,10 +22,7 @@
                     <div class="flex items-center">
                          <div class="shrink-0 flex items-center">
                               <a href="{{ url('/') }}" class="font-bold text-xl text-blue-600 flex items-center gap-2">
-                                   <svg class="w-8 h-8 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                                   </svg>
-                                   TamanAir
+                                   TASB 
                               </a>
                          </div>
                     </div>
